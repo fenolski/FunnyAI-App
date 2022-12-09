d9289212f1fb403fe6898b46058637df47856f78
fenolski <fenolski@gmail.com>
2022-12-09 01:23:18 +0100

    Odpytywanie SW Api i wybór bohatera

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from "react"
 
 export default function App() {
+
+  // function sendPicture() {
+  //   fetch('http://178.42.26.1:8888/')
+  //     .then(res => res.json())
+  //     .then(data => alert('Odebrano'))
+  //   alert('Naciśnięto przycisk')
+  // } 
+
+  const [heroId, setHeroId] = React.useState(1)
+
+    function sendPicture() {
+      fetch(`https://swapi.dev/api/people/${heroId}`)
+        .then(res => res.json())
+        .then(data => displayAPIAnswer(JSON.stringify(data)))      
+  }
+
+  function displayAPIAnswer(apiRes) {
+      alert(apiRes)
+  }
+
+  function nextHero() {
+      return setHeroId(oldHero => oldHero + 1)
+      }
+
+  function prevHero(){
+      return heroId > 1 && setHeroId(oldHero => oldHero - 1)
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Wybierz numer bohatera </Text>
+      <Text>Gwiezdnych Wojen: {heroId}</Text>
       <StatusBar style="auto" />
+      <Text />
+      <Button title=" + " onPress={() => nextHero()} />
+      <Button title="  - " onPress={() => prevHero()} />
+      <Text />
+      <Button title="Ask StarWars API" onPress={() => sendPicture()} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
